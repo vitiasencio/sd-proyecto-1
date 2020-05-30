f8b510665a42cbed8824b7c17b69d7d84982bbcf
Victor Asencio <vitiasencio@gmail.com>
2020-05-30 13:16:46 -0300

    exercise 2 ready

--- src/ejercicio-2/server.c
+++ src/ejercicio-2/server.c
@@ -9,18 +9,21 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-
 #define COMMAND "./tarea"
 #define BILLION 1000000000L
 
-/* Global variables for time measuring*/
-struct timespec start, end;
-unsigned long accum;
+int * calc_time_1_svc(void *argp, struct svc_req *rqstp){
 
+	static int  result;
 
-void local_calc_time(){
-
-	pid_t pid;
+	/*
+	 * insert server code here
+	 */
+	 
+	struct timespec start, end;
+	unsigned long accum;
+	 
+	 pid_t pid;
 
 	/* Registro tiempo inicial */
 	if(clock_gettime(CLOCK_REALTIME, &start)<0){
@@ -28,7 +31,8 @@ void local_calc_time(){
 	}
 	
 	if( (pid = fork() ) == 0 ){
-		
+	
+		/* Estoy en el hijo */
 		if( execl(COMMAND,NULL) < 0 ){
 			perror("Error en execl");
 			exit(-1);
@@ -47,17 +51,27 @@ void local_calc_time(){
 	}else{
 		accum = ( end.tv_sec - start.tv_sec )* BILLION + (BILLION - start.tv_nsec) + end.tv_nsec ; 
 	}
-		
-	printf("Tiempo ejecución en local con execl: %lu \n",accum);
+	
+	result = accum/1000;
 	
 	wait(NULL);
-	
+
+	return &result;
 	
 }
 
-void local_calc_time_system(){
+int * calc_time_system_1_svc(void *argp, struct svc_req *rqstp){
 
-	/* Registro tiempo inicial */
+	static int  result;
+
+	/*
+	 * insert server code here
+	 */
+	 
+	struct timespec start, end;
+	unsigned long accum;
+	 
+	 /* Registro tiempo inicial */
 	if(clock_gettime(CLOCK_REALTIME, &start)<0){
 		perror("Error en start");
 	}
@@ -75,64 +89,9 @@ void local_calc_time_system(){
 	}else{
 		accum = ( end.tv_sec - start.tv_sec )* BILLION + (BILLION - start.tv_nsec) + end.tv_nsec ; 
 	}
-	
-	printf("Tiempo ejecución en local con System: %lu \n",accum);
+	 
+	result = accum/1000;
 
-}
+	return &result;
 
-void time_1(char *host){
-
-	CLIENT *clnt;
-
-	clnt = clnt_create (host, TIME, TIMEVERS, "udp");
-
-
-	int  *result_1;
-	char *calc_time_1_arg;
-	int  *result_2;
-	char *calc_time_system_1_arg;
-
-	
-	if (clnt == NULL) {
-		clnt_pcreateerror (host);
-		exit (1);
-	}
-	
-	result_1 = calc_time_1((void*)&calc_time_1_arg, clnt);
-	
-	if (result_1 == (int *) NULL) {
-		clnt_perror (clnt, "call failed");
-	}
-	
-	result_2 = calc_time_system_1((void*)&calc_time_system_1_arg, clnt);
-	
-	if (result_2 == (int *) NULL) {
-		clnt_perror (clnt, "call failed");
-	}
-	
-	printf("numero system remoto: %i\n", *result_2);
-	
-	clnt_destroy (clnt);
-	
-}
-
-
-int main(int argc, char *argv[]) {
-	
-	char *host;
-
-	if (argc < 2) {
-		printf ("usage: %s server_host\n", argv[0]);
-		exit (1);
-	}
-	
-	host = argv[1];
-
-	local_calc_time();
-	local_calc_time_system();
-	
-	//time_1 (host);
-
-	exit (0);
-	
 }
