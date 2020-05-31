7fe7741e2d3e76c6101f46a58beba4754ffa509d
Ramiro Fraysse <ramirofraysse@gmail.com>
2020-05-31 19:32:09 -0300

    Del 4 me faltaria make nomas

---
+++ src/ejercicio-4/a/cliente.c
@@ -0,0 +1,85 @@
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+#define PORT 14550 /* El puerto donde se conectara */
+#define MAXDATASIZE 4
+#define TOPEPRUEBAS 10
+
+int main(int argc, char *argv[])
+{
+    struct timeval ti,tf;
+    double tiempo;
+    
+    int sockfd, numbytes;
+    char buf[MAXDATASIZE]; /* Buffer donde se reciben los datos */
+    struct hostent *he; /* Se utiliza para convertir el nombre del host
+    a su dirección IP */
+    struct sockaddr_in their_addr; /* dirección del server donde se
+    conectara */
+    if (argc != 2){
+        fprintf(stderr,"usage: client hostname\n");
+        exit(1);
+    }
+    if ((he=gethostbyname(argv[1])) == NULL) { 
+        herror("gethostbyname");
+        exit(1);
+    }
+    
+    int i;
+    double suma=0;
+    double promedio=0;
+    for(int i=0; i<TOPEPRUEBAS; i++){
+
+        /* Creamos el socket */
+        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+        {
+            perror("socket");
+            exit(1);
+        } 
+
+        /* Establecemos their_addr con la dirección del server */
+        their_addr.sin_family = AF_INET;
+        their_addr.sin_port = htons(PORT);
+        their_addr.sin_addr = *((struct in_addr *)he->h_addr);
+        bzero(&(their_addr.sin_zero), 8);
+        
+        gettimeofday(&ti,NULL);
+        /* Intentamos conectarnos con el servidor */
+        if (connect(sockfd, (struct sockaddr *)&their_addr, sizeof(struct
+        sockaddr)) == -1)
+        {
+            perror("connect");
+            exit(1);
+        }
+        
+        
+        /* Recibimos los datos del servidor */
+        if ((numbytes=recv(sockfd, buf, MAXDATASIZE, 0)) == -1)
+        {
+            perror("recv");
+            exit(1);
+        }
+        
+        gettimeofday(&tf,NULL);
+        tiempo = ((tf.tv_sec - ti.tv_sec)*1000000) + (tf.tv_usec - ti.tv_usec);
+        printf("tiempo %g us en recibir %d bytes: \n",tiempo,sizeof(buf));
+
+        suma = suma + tiempo;
+    }
+    promedio = suma/TOPEPRUEBAS;
+    /* Visualizamos lo recibido */
+    buf[numbytes] = '\0';
+    printf("Demora en promedio %g us en recibir %d bytes: \n",promedio,sizeof(buf));
+    
+    /* Devolvemos recursos al sistema */
+    close(sockfd);
+    return 0;
+} 
---
+++ src/ejercicio-4/a/servidor.c
@@ -0,0 +1,71 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/utsname.h>
+
+#define MYPORT 14550 /*Nro de puerto donde se conectaran los clientes, recomienda poner despues del 10000, gralmente antes estan reservados para distintas funciones*/
+#define BACKLOG 10 /* Tamaño de la cola de conexiones recibidas */
+
+main() {
+    int sockfd; /* El servidor escuchara por sockfd */
+    int newfd; /* las transferencias de datos se realizar mediante newfd */
+    struct sockaddr_in my_addr; /* contendrá la dir IP y el nro de puerto local */
+    struct sockaddr_in their_addr; //Contendrá la dir IP y nro de puerto del cliente
+    int sin_size; /* Contendra el tamaño de la escructura sockaddr_in */
+    
+    /****se crea el socket*****/
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Asigna valores a la estruct my_addr para luego poder llamar a la función bind()
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port = htons(MYPORT); /*formato de network byte order htons es para convertirlo para que se pueda transmitir por la red */
+    my_addr.sin_addr.s_addr = INADDR_ANY; /* automaticamente usa IP local */
+    bzero(&(my_addr.sin_zero), 8); /* rellena con ceros el resto de la estructura */
+
+    /*******BIND*****Asigna un nombre al socket */
+    if ( bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) == -1){
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    /********LISTEN***Habilita el socket para recibir conexiones, con una cola de x conexiones en espera como máximo */
+    if (listen(sockfd, BACKLOG) == -1){
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    /***********ACCEPT**************/
+    while(1) /* loop que llama a accept() */
+    {
+        sin_size = sizeof(struct sockaddr_in);
+        /*Se espera por conexiones ,*/
+        if ((newfd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size)) == -1)
+        {
+            perror("accept");
+            continue; /* Si se produce un error se finaliza el programa */
+        }
+        printf("server: conexion desde: %s\n", inet_ntoa(their_addr.sin_addr));
+        printf("Desde puerto: %d \n", ntohs(their_addr.sin_port));
+        
+        if (!fork())
+        { /* Comienza el proceso hijo, enviamos los datos mediante newfd */
+            if (send(newfd, "###\n",4, 0) == -1)
+                perror("send");
+            close(newfd);
+            exit(0);
+        }
+    }
+close(newfd);
+/* Se suspende la ejecucion del proceso padre hasta que finalice el proceso hijo */
+while(waitpid(-1,NULL,WNOHANG) > 0);
+/* Una vez finalizado el proceso hijo, se vuelve a llamar a accept() */
+}
